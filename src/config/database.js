936990995f5b3985485874c39d2cdf6834c4df15
sophiCardona <sophia.cardona39931@ucaldas.edu.co>
2025-10-10 22:40:17 -0500

    Configuracion base de datos para test

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,25 @@
+// database.js
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const connectDB = async() => {
-    try {
-        const conn = await mongoose.connect(process.env.DATABASE_URL);
-        console.log(`MongoDB Connected: ${conn.connection.name}`)
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
+const connectDB = async () => {
+  const { DATABASE_URL, DATABASE_URL_TEST, NODE_ENV } = process.env;
+
+  const connectString = NODE_ENV === 'test' 
+    ? DATABASE_URL_TEST 
+    : DATABASE_URL;
+
+  if (!connectString) {
+    throw new Error('❌ Verificar que el archivo .env tenga TODAS las variables de entorno definidas');
+  }
+
+  try {
+    const conn = await mongoose.connect(connectString);
+    console.log(`✅ MongoDB Connected: ${conn.connection.name} (${NODE_ENV})`);
+  } catch (error) {
+    console.error('❌ Error al conectar con MongoDB:', error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
